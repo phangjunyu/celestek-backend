ac81a929f93b49ae3dfeee5256f3c60581770d3f
Jason Goodman <jasonpgoodman@berkeley.edu>
2017-11-14 11:51:29 -0800

    Able to download images from Planet Labs dirtect to Amazon S3.

--- imageDownload/get_images.py
+++ imageDownload/get_images.py
@@ -2,12 +2,20 @@ import os
 import requests
 import pycurl
 import urllib.request
+import boto
+import boto.s3
+import json
+import io
 from multiprocessing.dummy import Pool as ThreadPool
 from stats_endpoint import result_data
 from search_endpoint import result
 from retrying import retry
 from osgeo import gdal
 from requests.auth import HTTPBasicAuth
+from boto.s3.key import Key
+
+with open('../config.json', 'r') as f:
+    config = json.load(f)
 
 #iterate over images collected for location by day collected and find the most recent date images were collected
 max_date = ''
@@ -74,20 +82,33 @@ thread_pool = ThreadPool(parallelism)
 # All items will be sent to the `activate_item` function but only 5 will be running at once
 thread_pool.map(activate_item, image_ids_to_download)
 
-def download_image(item_id):
+AWS_ACCESS_KEY_ID = config['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = config['AWS_SECRET_ACCESS_KEY']
+
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+bucket_name = config['AWS_BUCKET']
+#bucket = conn.create_bucket(bucket_name,
+    #location=boto.s3.connection.Location.DEFAULT)
+
+def download_and_upload_image(item_id):
 	for asset_type in asset_types:
 		item_url = 'https://api.planet.com/data/v1/item-types/{}/items/{}/assets'.format(item_type, item_id)
 		# Request a new download URL
 		result = requests.get(item_url, auth=HTTPBasicAuth(os.environ['PL_API_KEY'], ''))
 		download_url = result.json()[asset_type]['location']
 		if (asset_type == 'analytic') :
-			output_file = item_id + 'sjc_golfcourse.tif'
+			output_file = item_id + '_tif'
 		elif (asset_type == 'analytic_xml'):
-			output_file = item_id + 'sjc_golfcourse.xml'
-		urllib.request.urlretrieve(download_url, output_file)
-
+			output_file = item_id + '_xml'
+		#urllib.request.urlretrieve(download_url, output_file)
+		bucket = conn.get_bucket(bucket_name)
+		k = Key(bucket)
+		k.key = output_file
+		file_object = urllib.request.urlopen(download_url)
+		fp = io.BytesIO(file_object.read())
+		k.set_contents_from_file(fp)     
 
 for item_id in image_ids_to_download:
 	print(item_id)
-	download_image(item_id)
+	download_and_upload_image(item_id)
 
